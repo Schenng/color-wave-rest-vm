e549dd4cfeab547255848eeb2fe9e0ebfef06cea
schenng <scheng365@gmail.com>
2018-11-04 16:48:24 -0500

    Final model stuff

--- app.py
+++ app.py
@@ -29,13 +29,19 @@ def process():
                                             (0.5, 0.5, 0.5))]
     allTransforms = transforms.Compose(transform_list)
 
+    # Do transforms
     A_img = allTransforms(A_img)
     A_img = A_img[0, ...] * 0.299 + A_img[1, ...] * 0.587 + A_img[2, ...] * 0.114
-    
+
+    #Some more transforms?
     A_img = torch.stack([A_img] * 3, dim = 0).unsqueeze(0)
 
+    # Put the image through the model
     MODEL.set_input({'A': A_img, 'A_paths' : ''})
-    MODEL.test()
+    MODEL.test()    
+    visuals = MODEL.get_current_visuals()
+
+    save_images(visuals, 'bucket-fuse/', aspect_ratio=1.0, width=256)
 
     return 'process'
 
@@ -51,3 +57,28 @@ def image():
 if __name__ == '__main__':
     #app.run(debug=True)
     app.run()
+
+def tensor2im(input_image, imtype=np.uint8):
+    if isinstance(input_image, torch.Tensor):
+        image_tensor = input_image.data
+    else:
+        return input_image
+    image_numpy = image_tensor[0].cpu().float().numpy()
+    if image_numpy.shape[0] == 1:
+        image_numpy = np.tile(image_numpy, (3, 1, 1))
+    image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
+    return image_numpy.astype(imtype)
+
+def save_images(visuals, image_path, aspect_ratio=1.0, width=256):
+
+    for label, im_data in visuals.items():
+        im = tensor2im(im_data)
+        image_name = '%s.png' % (label)
+        h, w, _ = im.shape
+        if aspect_ratio > 1.0:
+            im = imresize(im, (h, int(w * aspect_ratio)), interp='bicubic')
+        if aspect_ratio < 1.0:
+            im = imresize(im, (int(h / aspect_ratio), w), interp='bicubic')
+
+        image_pil = Image.fromarray(im)
+        image_pil.save(image_path + image_name)
