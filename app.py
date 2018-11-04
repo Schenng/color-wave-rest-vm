719cacf8a6b42298e6a6035f1d8084c18eaf2773
schenng <scheng365@gmail.com>
2018-11-04 15:30:17 -0500

    Add image + initial process script

--- app.py
+++ app.py
@@ -11,6 +11,21 @@ app = Flask(__name__)
 def index():
     return "Hello, World 2!"
 
+@app.route('/process')
+def process():
+
+    #Load an image
+    A_path = './images/img_0001.jpg'
+    A_img = Image.open(A_path).convert('RGB')
+    ratio = A_img.width / A_img.height
+
+    #Create transforms
+    transform_list = [transforms.Resize((256,256)), transforms.ToTensor(),
+                       transforms.Normalize((0.5, 0.5, 0.5),
+                                            (0.5, 0.5, 0.5))]
+    allTransforms = transforms.Compose(transform_list)
+
+    return 'process image'
 @app.route('/image')
 def image():
     return "Image Response"
