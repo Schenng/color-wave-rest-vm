162edbeea46baef6ca41da88bc6caa6105b5b8bc
Simon Cheng <scheng365@gmail.com>
2018-11-04 19:45:04 +0000

    Added gitingore

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return "Hello, World 2!"
 
+@app.route('/image')
+def image():
+    return "Image Response"
+
 if __name__ == '__main__':
     #app.run(debug=True)
     app.run()
