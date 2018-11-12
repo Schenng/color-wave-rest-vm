1af494e74428f69fa152432448176f6495ff65e5
schenng <scheng365@gmail.com>
2018-11-12 18:36:36 -0500

    Switch back server port

--- app.py
+++ app.py
@@ -202,5 +202,5 @@ def fuse():
     return send_file(filename, mimetype='image/gif')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
     #app.run()
